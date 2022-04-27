8dadca1a9db580e62e881968c748b1c2a5cc0efd
catintheboxx <wased1009@gmail.com>
2022-04-28 00:02:53 +0700

    [add]Front-end app

---
+++ oasip-front-app/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <Navbar />
+  <div class="mx-24">
+    <router-view></router-view>
+  </div>
+  <Footer />
+</template>
+ 
+<script setup>
+import Navbar from './components/Navbar.vue';
+import Footer from './components/Footer.vue';
+</script>
+ 
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+
+body {
+  font-family: 'Poppins', sans-serif;
+  background-color: #F9FAFB;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #23374D;
+  overscroll-behavior: auto;
+}
+
+.l-color-navi {
+  color: #23374D;
+}
+
+.l-border-b-blue {
+  border-bottom: 4px solid #1089FF;
+}
+
+.l-bg-blue {
+  background-color: #1089FF;
+}
+
+.l-w-408 {
+  width: 408px;
+}
+</style>
---
+++ oasip-front-app/src/components/Footer.vue
@@ -0,0 +1,34 @@
+<template>
+    <footer>
+        <div class="flex space-x-6 h-80 bg-white place-content-center place-items-center drop-shadow-md">
+            <div class="flex l-w-408 h-32 place-content-center place-items-center space-x-6">
+                <div class="w-24 h-24 l-bg-blue inline-block rounded-full"></div>
+                <div class="inline-block">
+                    <p class="text-xs font-semibold">63130500026</p>
+                    <p class="text-2xl font-semibold">Chotiwit Souyan</p>
+                </div>
+            </div>
+            <div class="flex l-w-408 h-32 place-content-center place-items-center space-x-6">
+                <div class="w-24 h-24 l-bg-blue inline-block rounded-full"></div>
+                <div class="inline-block">
+                    <p class="text-xs font-semibold">63130500034</p>
+                    <p class="text-2xl font-semibold">Nuttida Meeboon</p>
+                </div>
+            </div>
+            <div class="flex l-w-408 h-32 place-content-center place-items-center space-x-6">
+                <div class="w-24 h-24 l-bg-blue inline-block rounded-full"></div>
+                <div class="inline-block">
+                    <p class="text-xs font-semibold">63130500065</p>
+                    <p class="text-2xl font-semibold">Narawich Kampusa</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+</template>
+ 
+<script setup>
+
+</script>
+ 
+<style>
+</style>
