8416bd37e8fa6c74c7e079c878ab1202805e2b1a
xbklyn <nut.chotiwit@gmail.com>
2022-04-30 02:39:04 +0700

    [edit] controller

--- oasip-app/src/main/java/sit/int221/oasip/controllers/EventsController.java
+++ oasip-app/src/main/java/sit/int221/oasip/controllers/EventsController.java
@@ -24,7 +24,7 @@ public class EventsController {
         return erepo.findAll();
     }
 
-    @GetMapping("/category/{categoryId")
+    @GetMapping("/category/{categoryId}")
     public List<Event> getEventByCategory(
             @PathVariable Integer id
     ){
