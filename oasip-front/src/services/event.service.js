b1909b758eb2842c4fb619b51a6c8f9cf92cef8c
xbklyn <nut.chotiwit@gmail.com>
2022-04-29 16:11:50 +0700

    [edit] url fetch

--- oasip-front/src/services/event.service.js
+++ oasip-front/src/services/event.service.js
@@ -1,5 +1,5 @@
 export const getAllEvents = async ()  => {
-    const res = await fetch('http://oasipbe/api/events')
+    const res = await fetch('http://localhost:9999events')
     if (res.status === 200) {
         console.log(res.status);
         return await res.json();
