6d10675983a905bae57dc8c143030e5c9d4ebb7d
xbklyn <nut.chotiwit@gmail.com>
2022-04-30 02:16:29 +0700

    [edit] delete unused import

--- oasip-app/src/main/java/sit/int221/oasip/controllers/EventsController.java
+++ oasip-app/src/main/java/sit/int221/oasip/controllers/EventsController.java
@@ -1,6 +1,5 @@
 package sit.int221.oasip.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
