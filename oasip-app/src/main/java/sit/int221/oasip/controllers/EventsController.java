e84fb2782d2c62d9e05a248bb40473445bdba1da
xbklyn <nut.chotiwit@gmail.com>
2022-04-30 21:55:40 +0700

    [add] api find evnet by id

--- oasip-app/src/main/java/sit/int221/oasip/controllers/EventsController.java
+++ oasip-app/src/main/java/sit/int221/oasip/controllers/EventsController.java
@@ -1,13 +1,17 @@
 package sit.int221.oasip.controllers;
 
+import org.springframework.http.HttpStatus;
 // import org.springframework.web.bind.annotation.GetMapping;
 // import org.springframework.web.bind.annotation.PathVariable;
 // import org.springframework.web.bind.annotation.RequestMapping;
 // import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
 import sit.int221.oasip.entities.Event;
 import sit.int221.oasip.repositories.EventRepository;
 
+import java.net.http.HttpResponse;
 import java.util.List;
 
 @CrossOrigin("*")
@@ -26,6 +30,16 @@ public class EventsController {
         return erepo.findAll();
     }
 
+
+    @GetMapping("/api/events/{id}")
+    public Event getEventById(
+        @PathVariable Integer id
+    ) {
+        return erepo.findById(id).orElseThrow(() -> {
+            new ResponseStatusException(HttpStatus.NOT_FOUND);
+        });
+    }
+    
     @GetMapping("/category/{categoryId}")
     public List<Event> getEventByCategory(
             @PathVariable Integer categoryId
