747b51f0c4cd1bcdc2b8cdce8e6c8d0d5ba6b5e5
catintheboxx <wased1009@gmail.com>
2022-04-30 19:47:36 +0700

    test

--- oasip-front/vite.config.js
+++ oasip-front/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     server: {
         proxy: {
             '/api': {
-                target: 'http://10.4.56.107:8080',
+                target: 'http://localhost:8080',
                 changeOrigin: true,
                 secure: false,
                 rewrite: (path) => path.replace(/^\/api/, '')
