b651ea88287f3f19288e6b4bebf9b234f06434fc
xbklyn <nut.chotiwit@gmail.com>
2022-04-30 20:56:53 +0700

    Revert "edit cross origin"
    This reverts commit 5aa021878d173b03510d523b501b5f6b98882907.

--- oasip-app/src/main/java/sit/int221/oasip/controllers/EventsController.java
+++ oasip-app/src/main/java/sit/int221/oasip/controllers/EventsController.java
@@ -1,6 +1,9 @@
 package sit.int221.oasip.controllers;
 
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import sit.int221.oasip.entities.Event;
 import sit.int221.oasip.repositories.EventRepository;
 
@@ -8,7 +11,6 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/events")
-@CrossOrigin("*")
 public class EventsController {
 
     private final EventRepository erepo;
