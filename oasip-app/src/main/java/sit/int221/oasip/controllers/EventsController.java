5aa021878d173b03510d523b501b5f6b98882907
xbklyn <nut.chotiwit@gmail.com>
2022-04-30 20:32:51 +0700

    edit cross origin

--- oasip-app/src/main/java/sit/int221/oasip/controllers/EventsController.java
+++ oasip-app/src/main/java/sit/int221/oasip/controllers/EventsController.java
@@ -1,9 +1,6 @@
 package sit.int221.oasip.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import sit.int221.oasip.entities.Event;
 import sit.int221.oasip.repositories.EventRepository;
 
@@ -11,6 +8,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/events")
+@CrossOrigin("*")
 public class EventsController {
 
     private final EventRepository erepo;
