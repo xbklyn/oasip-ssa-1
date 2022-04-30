b94bd5b46e1251ac4ca9022c839873b0a7448b0b
xbklyn <nut.chotiwit@gmail.com>
2022-04-30 21:12:41 +0700

    revert

--- oasip-app/src/main/java/sit/int221/oasip/controllers/EventsController.java
+++ oasip-app/src/main/java/sit/int221/oasip/controllers/EventsController.java
@@ -1,16 +1,28 @@
 package sit.int221.oasip.controllers;
 
+<<<<<<< HEAD
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+=======
+// import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.PathVariable;
+// import org.springframework.web.bind.annotation.RequestMapping;
+// import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+>>>>>>> parent of a0a1acc ([Test] Kuy 101 Not Found)
 import sit.int221.oasip.entities.Event;
 import sit.int221.oasip.repositories.EventRepository;
 
 import java.util.List;
 
+<<<<<<< HEAD
 @CrossOrigin("*")
+=======
+@CrossOrigin(origins = "http://10.4.56.107/")
+>>>>>>> parent of a0a1acc ([Test] Kuy 101 Not Found)
 @RestController
 @RequestMapping("/api/events")
 public class EventsController {
