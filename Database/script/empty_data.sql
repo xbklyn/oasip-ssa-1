ed45fdd00db39b97e42acf674f59fbd876ca6396
Nuttida Meeboon <nuttida.donut@mail.kmutt.ac.th>
2022-05-03 17:10:32 +0700

    [Edit] database

--- Database/script/empty_data.sql
+++ Database/script/empty_data.sql
@@ -5,7 +5,7 @@ use oasip;
 -- Table structure for table `eventcategories`
 --
 
-DROP TABLE IF EXISTS `eventcategories`;
+DROP TABLE IF EXISTS `eventCategories`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `eventcategories` (
