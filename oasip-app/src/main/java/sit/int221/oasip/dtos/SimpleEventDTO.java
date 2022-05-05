a546a0705a4e32463443ce5d7d0c12919dcb3c6d
xbklyn <nut.chotiwit@gmail.com>
2022-05-05 23:03:45 +0700

    [edit] ass filter function

--- oasip-app/src/main/java/sit/int221/oasip/dtos/SimpleEventDTO.java
+++ oasip-app/src/main/java/sit/int221/oasip/dtos/SimpleEventDTO.java
@@ -3,10 +3,6 @@ package sit.int221.oasip.dtos;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Column;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import java.sql.Time;
 import java.util.Date;
 
@@ -17,6 +13,6 @@ public class SimpleEventDTO {
     private Date eventDate;
     private Time eventStartTime;
     private Integer eventDuration;
-    private String eventCategoryName;
+    private Integer categoryId;
     private String eventNotes;
 }
