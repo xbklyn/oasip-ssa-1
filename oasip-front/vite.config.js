49553eb34415199cbd15cd29f0f67fecf5bac211
catintheboxx <wased1009@gmail.com>
2022-05-09 19:39:58 +0700

    test

--- oasip-front/vite.config.js
+++ oasip-front/vite.config.js
@@ -2,17 +2,40 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [vue()],
-    base: '/ssa1/',
-    server: {
-        proxy: {
-            '/api': {
-                target: 'http://intproj21.sit.kmutt.ac.th:8080/ssa1',
-                changeOrigin: true,
-                secure: false,
-                rewrite: (path) => path.replace(/^\/api/, '')
+// export default defineConfig({
+//     plugins: [vue()],
+//     base: '/ssa1/',
+//     server: {
+//         proxy: {
+//             '/api': {
+//                 target: 'http://intproj21.sit.kmutt.ac.th:8080/ssa1',
+//                 changeOrigin: true,
+//                 secure: false,
+//                 rewrite: (path) => path.replace(/^\/api/, '')
+//             }
+//         }
+//     }
+// })
+const defineConfig = {
+        plugins: [vue()],
+        base: '/ssa1/',
+    }
+
+export default defineConfig(({ command, mode }) => {
+  if (command === 'serve') {
+    return {
+        server: {
+            proxy: {
+                '/api': {
+                    target: 'http://intproj21.sit.kmutt.ac.th:8080/ssa1',
+                    changeOrigin: true,
+                    secure: false,
+                    rewrite: (path) => path.replace(/^\/api/, '')
+                }
             }
         }
     }
+  } else {
+    return defineConfig
+  }
 })
