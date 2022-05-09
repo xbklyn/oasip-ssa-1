0c972ecd4228ecb981ab4340ef50921159745b63
catintheboxx <wased1009@gmail.com>
2022-05-09 19:25:45 +0700

    test

--- oasip-front/vite.config.js
+++ oasip-front/vite.config.js
@@ -4,15 +4,15 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [vue()],
-    base: '/ssa1/'
-    // server: {
-    //     proxy: {
-    //         '/api': {
-    //             target: 'http://intproj21.sit.kmutt.ac.th:8080/ssa1',
-    //             changeOrigin: true,
-    //             secure: false,
-    //             rewrite: (path) => path.replace(/^\/api/, '')
-    //         }
-    //     }
-    // }
+    base: '/ssa1/',
+    server: {
+        proxy: {
+            '/api': {
+                target: 'http://intproj21.sit.kmutt.ac.th:8080/ssa1/api/',
+                changeOrigin: true,
+                secure: false,
+                rewrite: (path) => path.replace(/^\/api/, '')
+            }
+        }
+    }
 })
