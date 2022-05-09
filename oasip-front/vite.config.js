e1b5dc2b8e91f55e4743b61dba1b6b020ced6635
catintheboxx <wased1009@gmail.com>
2022-05-09 19:50:22 +0700

    p

--- oasip-front/vite.config.js
+++ oasip-front/vite.config.js
@@ -2,42 +2,18 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-// export default defineConfig({
-//     plugins: [vue()],
-//     base: '/ssa1/',
-//     server: {
-//         proxy: {
-//             '/api': {
-//                 target: 'http://intproj21.sit.kmutt.ac.th:8080/ssa1',
-//                 changeOrigin: true,
-//                 secure: false,
-//                 rewrite: (path) => path.replace(/^\/api/, '')
-//             }
-//         }
-//     }
-// })
-const defaultConfig = {
-        plugins: [vue()],
-        base: '/ssa1/',
-    }
-
-export default defineConfig(({ command, mode }) => {
-  if (command === 'build') {
-    return {
-        plugins: [vue()],
-        base: '/ssa1/',
-        server: {
-            proxy: {
-                '/api': {
-                    target: 'http://intproj21.sit.kmutt.ac.th:8080/ssa1',
-                    changeOrigin: true,
-                    secure: false,
-                    rewrite: (path) => path.replace(/^\/api/, '')
-                }
+export default defineConfig({
+    plugins: [vue()],
+    base: '/ssa1/',
+    server: {
+        proxy: {
+            '/api': {
+                target: 'http://intproj21.sit.kmutt.ac.th:8080/ssa1',
+                changeOrigin: true,
+                secure: false,
+                rewrite: (path) => path.replace(/^\/api/, '')
             }
         }
     }
-  } else {
-    return defaultConfig
-  }
 })
+
