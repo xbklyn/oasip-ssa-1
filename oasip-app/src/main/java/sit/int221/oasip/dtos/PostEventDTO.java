56f173c7b913e8efefc922fadf121840b2376432
xbklyn <nut.chotiwit@gmail.com>
2022-05-17 12:31:38 +0700

    [edit] add validation for post method

--- oasip-app/src/main/java/sit/int221/oasip/dtos/PostEventDTO.java
+++ oasip-app/src/main/java/sit/int221/oasip/dtos/PostEventDTO.java
@@ -2,14 +2,21 @@ package sit.int221.oasip.dtos;
 
 import lombok.Getter;
 import lombok.Setter;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
 import java.util.Date;
 
 @Getter @Setter
 public class PostEventDTO {
-
+    @NotNull
     private String bookingName;
+    @NotNull
+    @Email
     private String bookingEmail;
+    @NotNull
     private Date eventStartTime;
+    @NotNull
     private Integer categoryId;
     private String eventNotes;
 }
