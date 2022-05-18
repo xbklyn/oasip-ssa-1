5a18e2f73248c694b6f8ac618a76c837090cf45b
xbklyn <nut.chotiwit@gmail.com>
2022-05-18 16:01:30 +0700

    [add] entity to return start time at specific category and date

---
+++ oasip-app/src/main/java/sit/int221/oasip/dtos/TimeDTO.java
@@ -0,0 +1,14 @@
+package sit.int221.oasip.dtos;
+
+
+public class TimeDTO {
+    private String eventStartTime;
+
+    public String getEventStartTime() {
+        return eventStartTime;
+    }
+
+    public void setEventStartTime(String eventStartTime) {
+        this.eventStartTime = eventStartTime;
+    }
+}
