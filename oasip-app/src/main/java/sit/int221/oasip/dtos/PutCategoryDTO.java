aefc3b58ba52f1fa38b5d278ef2e7958eebc22f5
xbklyn <nut.chotiwit@gmail.com>
2022-05-20 16:46:04 +0700

    [add] DTO for edit category

---
+++ oasip-app/src/main/java/sit/int221/oasip/dtos/PutCategoryDTO.java
@@ -0,0 +1,28 @@
+package sit.int221.oasip.dtos;
+
+import javax.validation.constraints.NotNull;
+
+public class PutCategoryDTO {
+
+    @NotNull
+    private String eventCategoryName;
+
+    @NotNull
+    private Integer eventCategoryDuration;
+
+    public String getEventCategoryName() {
+        return eventCategoryName;
+    }
+
+    public void setEventCategoryName(String eventCategoryName) {
+        this.eventCategoryName = eventCategoryName;
+    }
+
+    public Integer getEventCategoryDuration() {
+        return eventCategoryDuration;
+    }
+
+    public void setEventCategoryDuration(Integer eventCategoryDuration) {
+        this.eventCategoryDuration = eventCategoryDuration;
+    }
+}
