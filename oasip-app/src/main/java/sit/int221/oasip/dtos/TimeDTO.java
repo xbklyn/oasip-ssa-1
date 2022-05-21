bf3bfbd2aa473d8cccd15657c87499c880e954eb
xbklyn <nut.chotiwit@gmail.com>
2022-05-21 18:07:32 +0700

    [edit] add getter and setter

--- oasip-app/src/main/java/sit/int221/oasip/dtos/TimeDTO.java
+++ oasip-app/src/main/java/sit/int221/oasip/dtos/TimeDTO.java
@@ -7,6 +7,14 @@ public class TimeDTO {
     private Date eventStartTime;
     private Date eventEndTime;
 
+    public Date getEventEndTime() {
+        return eventEndTime;
+    }
+
+    public void setEventEndTime(Date eventEndTime) {
+        this.eventEndTime = eventEndTime;
+    }
+
     public Date getEventStartTime() {
         return eventStartTime;
     }
