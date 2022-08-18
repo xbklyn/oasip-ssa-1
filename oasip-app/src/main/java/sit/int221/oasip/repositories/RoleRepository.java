d286336fd0a8f551b601fd4cca641e0d32f2cdec
xbklyn <nut.chotiwit@gmail.com>
2022-08-18 11:16:47 -0400

    [add] rest controller for role and clinic registration

---
+++ oasip-app/src/main/java/sit/int221/oasip/repositories/RoleRepository.java
@@ -0,0 +1,7 @@
+package sit.int221.oasip.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import sit.int221.oasip.entities.Role;
+
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+}
