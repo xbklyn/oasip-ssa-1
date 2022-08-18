06b63bda32d0783d315b8270ffc3b70fdfe89776
catintheboxx <wased1009@gmail.com>
2022-08-19 01:16:28 +0700

    add:homepage responsive

--- oasip-front/tailwind.config.js
+++ oasip-front/tailwind.config.js
@@ -1,20 +1,23 @@
 module.exports = {
-    content: [
-        "./index.html",
-        "./src/**/*.{vue,js,ts,jsx,tsx}",
-    ],
-    theme: {
-        extend: {},
-        screens: {
-            'tablet': '640px',
-            // => @media (min-width: 640px) { ... }
-
-            'laptop': '1024px',
-            // => @media (min-width: 1024px) { ... }
-
-            'desktop': '1280px',
-            // => @media (min-width: 1280px) { ... }
-        },
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  theme: {
+    extend: {},
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
     },
-    plugins: [],
-}
+    // screens: {
+    //     'tablet': '640px',
+    //     // => @media (min-width: 640px) { ... }
+
+    //     'laptop': '1024px',
+    //     // => @media (min-width: 1024px) { ... }
+
+    //     'desktop': '1280px',
+    //     // => @media (min-width: 1280px) { ... }
+    // },
+  },
+  plugins: [],
+};
