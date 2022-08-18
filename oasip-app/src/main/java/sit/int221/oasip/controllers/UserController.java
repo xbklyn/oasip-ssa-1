dd56deaaaeda2a5b130f91d8b989d5bbb431b1dc
xbklyn <nut.chotiwit@gmail.com>
2022-08-18 12:26:03 -0400

    [add] REST API GET Method

--- oasip-app/src/main/java/sit/int221/oasip/controllers/UserController.java
+++ oasip-app/src/main/java/sit/int221/oasip/controllers/UserController.java
@@ -1,13 +1,12 @@
 package sit.int221.oasip.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import sit.int221.oasip.dtos.SimpleUserDTO;
 import sit.int221.oasip.entities.User;
 import sit.int221.oasip.services.UserService;
 
+import java.time.Instant;
 import java.util.List;
 
 @RestController
@@ -20,16 +19,30 @@ public class UserController {
         this.userService = userService;
     }
 
-    //GET
+    //GET all
     @GetMapping("")
     public List<User> getAllUser() {
         return userService.getAll();
     }
 
+    //GET by ID
     @GetMapping("/{id}")
-    public User getUserById(
-            @PathVariable Integer id
-    ){
-        return userService.getById(id);
-    }
+    public User getUserById(@PathVariable Integer id)
+    {return userService.getById(id);}
+
+    //GET by role
+    @GetMapping("/role/{role}")
+    public List<SimpleUserDTO> getUserByRole(@PathVariable String role)
+    {return userService.getByRole(role);}
+
+    //GET by Create On
+    @GetMapping("/create/{date}")
+    public List<SimpleUserDTO> getUserByCreatedOn(@PathVariable String date)
+    {return userService.getByCreate(date);}
+
+    //GET by Update On
+    @GetMapping("/update/{date}")
+    public List<SimpleUserDTO> getUserByUpdateOn(@PathVariable String date)
+    {return userService.getByUpdate(date);}
+
 }
--- oasip-app/src/main/java/sit/int221/oasip/repositories/UserRepository.java
+++ oasip-app/src/main/java/sit/int221/oasip/repositories/UserRepository.java
@@ -1,9 +1,22 @@
 package sit.int221.oasip.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import sit.int221.oasip.entities.Role;
 import sit.int221.oasip.entities.User;
+
+import java.time.Instant;
+import java.util.List;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
 
-    public User findByUserName(String username);
+    public List<User> findByRole(Role role);
+
+    @Query(value = "select * from users where date(createdOn) = :createdOn" , nativeQuery = true)
+    public List<User> findByCreatedOn(
+            @Param("createdOn") String createdOn);
+
+    @Query(value = "select * from users where date(updatedOn) = :updatedOn" , nativeQuery = true)
+    public List<User> findByUpdatedOn(@Param("updatedOn") String date);
 }
