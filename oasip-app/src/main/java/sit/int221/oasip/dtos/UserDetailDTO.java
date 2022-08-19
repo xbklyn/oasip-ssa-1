e551cc05ddbf719e4f2eeafefeed9f02452009c2
xbklyn <nut.chotiwit@gmail.com>
2022-08-19 01:11:40 -0400

    add:dto for detail page for fe

---
+++ oasip-app/src/main/java/sit/int221/oasip/dtos/UserDetailDTO.java
@@ -0,0 +1,52 @@
+package sit.int221.oasip.dtos;
+
+import java.time.Instant;
+
+public class UserDetailDTO {
+
+    private String userName;
+    private String userEmail;
+    private Instant createdOn;
+    private Instant updatedOn;
+    private String role;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public Instant getCreatedOn() {
+        return createdOn;
+    }
+
+    public void setCreatedOn(Instant createdOn) {
+        this.createdOn = createdOn;
+    }
+
+    public Instant getUpdatedOn() {
+        return updatedOn;
+    }
+
+    public void setUpdatedOn(Instant updatedOn) {
+        this.updatedOn = updatedOn;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
