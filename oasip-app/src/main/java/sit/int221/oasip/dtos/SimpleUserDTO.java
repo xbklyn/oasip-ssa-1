b7e9540f159db7c7adafbac095b365bc0f57331d
xbklyn <nut.chotiwit@gmail.com>
2022-08-19 02:49:58 -0400

    edit:add validation

--- oasip-app/src/main/java/sit/int221/oasip/dtos/SimpleUserDTO.java
+++ oasip-app/src/main/java/sit/int221/oasip/dtos/SimpleUserDTO.java
@@ -1,15 +1,25 @@
 package sit.int221.oasip.dtos;
 
 import org.joda.time.DateTime;
+import org.modelmapper.internal.bytebuddy.implementation.bind.annotation.Empty;
 import sit.int221.oasip.entities.Role;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.time.Instant;
 import java.util.Date;
 
 public class SimpleUserDTO {
+    @NotNull
+    @Size(min = 1 , max = 100 , message = "Size must be between 1 and 100")
     private String userName;
+
+    @NotNull
+    @Size(min = 1 , max = 50 , message = "Size must be between 1 and 50")
     private String userEmail;
+
     private String role;
 
     public String getUserName() {
