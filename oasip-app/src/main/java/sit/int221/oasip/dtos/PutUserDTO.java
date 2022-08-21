c8969b7c596c0f1683eeb4cbb2ba9744f12931c3
xbklyn <nut.chotiwit@gmail.com>
2022-08-21 11:59:50 -0400

    add:new dto for put method

---
+++ oasip-app/src/main/java/sit/int221/oasip/dtos/PutUserDTO.java
@@ -0,0 +1,41 @@
+package sit.int221.oasip.dtos;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+public class PutUserDTO {
+    @NotNull
+    @Size(min = 1 , max = 100 , message = "Size must be between 1 and 100")
+    private String userName;
+
+    @NotNull
+    @Size(min = 1 , max = 50 , message = "Size must be between 1 and 50")
+    private String userEmail;
+
+    @NotNull
+    private String role;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
