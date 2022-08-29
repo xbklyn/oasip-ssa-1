723a8cb8a3c4c0571acd2981b7e4b74b65a78eba
catintheboxx <wased1009@gmail.com>
2022-08-29 16:28:06 +0700

    add:login btn

--- oasip-front/src/views/LoginPage.vue
+++ oasip-front/src/views/LoginPage.vue
@@ -14,23 +14,23 @@
             class="w-full h-12 pl-2 text-sm bg-transparent border-2 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer"
           />
           <p v-if="responeError" class="absolute text-sm text-red-500 ml-2">
-            {{ responeError }}
+            {{ responeError?.details?.userEmail }}
           </p>
         </div>
         <div>
           <p class="text-base">Password</p>
           <input
-            v-model="login.password"
+            v-model="login.rawPassword"
             type="password"
             class="w-full h-12 pl-2 text-sm bg-transparent border-2 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer"
           />
           <p v-if="responeError" class="absolute text-sm text-red-500 ml-2">
-            {{ responeError }}
+            {{ responeError?.details?.password }}
           </p>
         </div>
         <div>
           <div
-            class="h-12 place-content-center flex items-center justify-center l-bg-blue"
+            class="h-12 place-content-center flex items-center justify-center l-bg-blue hover:bg-[#1089ff]/70 duration-150"
           >
             <button
               @click="userLogin"
@@ -40,7 +40,12 @@
             </button>
           </div>
           <div class="h-12 flex items-center justify-center">
-            <p class="text-xs font-light text-center l-color-gray">Back</p>
+            <button
+              @click="backToHome"
+              class="w-full h-12 text-xs font-light text-center text-[#8d8d8d] hover:text-[#d4daef]/70 duration-150"
+            >
+              Back
+            </button>
           </div>
         </div>
       </div>
@@ -55,11 +60,12 @@ import { useRouter } from 'vue-router';
 const myRouter = useRouter();
 const login = ref({
   email: '',
-  password: '',
+  rawPassword: '',
 });
 const responeError = ref({});
 
 const userLogin = async () => {
+  responeError.value = {};
   await fetch(`${import.meta.env.VITE_BASE_URL}/match`, {
     method: 'POST',
     headers: {
@@ -69,11 +75,17 @@ const userLogin = async () => {
   }).then(async (res) => {
     if (res.ok) {
       myRouter.push({
-        name: 'Homepage',
+        name: 'Home',
       });
     } else {
       return (responeError.value = await res.json());
     }
+  });
+};
+
+const backToHome = () => {
+  myRouter.push({
+    name: 'Home',
   });
 };
 </script>
