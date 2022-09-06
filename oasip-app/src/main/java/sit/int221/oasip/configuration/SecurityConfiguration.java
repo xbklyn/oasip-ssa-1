6da42803aeb6dd2d0906e4eac5643e6d144f3216
xbklyn <nut.chotiwit@gmail.com>
2022-09-06 15:22:04 +0700

    edit:add entrypoint

--- oasip-app/src/main/java/sit/int221/oasip/configuration/SecurityConfiguration.java
+++ oasip-app/src/main/java/sit/int221/oasip/configuration/SecurityConfiguration.java
@@ -2,7 +2,6 @@ package sit.int221.oasip.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -10,6 +9,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import sit.int221.oasip.JwtAuthenticationEntryPoint;
 import sit.int221.oasip.filter.JWTFilter;
 import sit.int221.oasip.services.JwtService;
 
@@ -19,10 +19,12 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     private final JwtService jwtService;
     private final JWTFilter jwtFilter;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
 
-    public SecurityConfiguration(JwtService jwtService, JWTFilter jwtFilter) {
+    public SecurityConfiguration(JwtService jwtService, JWTFilter jwtFilter, JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint) {
         this.jwtService = jwtService;
         this.jwtFilter = jwtFilter;
+        this.jwtAuthenticationEntryPoint = jwtAuthenticationEntryPoint;
     }
 
     @Override
@@ -48,6 +50,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .anyRequest()
                 .authenticated()
                 .and()
+                .exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint).and()
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
