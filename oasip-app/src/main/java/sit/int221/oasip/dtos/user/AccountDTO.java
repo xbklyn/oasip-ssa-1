40495d381c257bddfb81b57249ac372b70ab564c
xbklyn <nut.chotiwit@gmail.com>
2022-09-26 20:54:39 +0700

    add:for get after login

---
+++ oasip-app/src/main/java/sit/int221/oasip/dtos/user/AccountDTO.java
@@ -0,0 +1,40 @@
+package sit.int221.oasip.dtos.user;
+
+public class AccountDTO {
+    private Integer userId;
+    private String userName;
+    private String email;
+    private String role;
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
