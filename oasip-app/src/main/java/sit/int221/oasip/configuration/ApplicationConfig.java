f46c62a944228ff99b6e596788c122382d6cb266
xbklyn <nut.chotiwit@gmail.com>
2022-10-01 22:34:08 +0700

    edit:create bean for email sending

--- oasip-app/src/main/java/sit/int221/oasip/configuration/ApplicationConfig.java
+++ oasip-app/src/main/java/sit/int221/oasip/configuration/ApplicationConfig.java
@@ -3,9 +3,13 @@ package sit.int221.oasip.configuration;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import sit.int221.oasip.utils.ListMapper;
+
+import java.util.Properties;
 
 @Configuration
 public class ApplicationConfig {
@@ -20,4 +24,22 @@ public class ApplicationConfig {
 
     @Bean
     public Argon2PasswordEncoder passwordEncoder(){ return  new Argon2PasswordEncoder(16,24,1,1 << 12,3);}
+
+    @Bean
+    public JavaMailSender getJavaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587);
+
+        mailSender.setUsername("oasip.ssa1@gmail.com");
+        mailSender.setPassword("nrexcshttjhqsixk");
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.debug", "true");
+
+        return mailSender;
+    }
 }
