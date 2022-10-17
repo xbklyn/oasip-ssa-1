c71dd15b32b73475e1d4f0ac4b7bc7951a847b64
xbklyn <nut.chotiwit@gmail.com>
2022-10-17 13:13:16 +0700

    add:dto with file to return FIleURL and FileName and create getMapping to download a file from URL

---
+++ oasip-app/src/main/java/sit/int221/oasip/dtos/event/DetailEventWithFileDTO.java
@@ -0,0 +1,145 @@
+package sit.int221.oasip.dtos.event;
+
+import sit.int221.oasip.entities.Event;
+
+import java.util.Date;
+
+public class DetailEventWithFileDTO {
+    private Integer id;
+    private String bookingName;
+    private String bookingEmail;
+    private Date eventStartTime;
+    private Date eventEndTime;
+    private Integer eventDuration;
+    private Integer categoryId;
+    private String eventCategoryName;
+    private String eventCategoryDescription;
+    private String eventCategoryDuration;
+    private String eventNotes;
+    private Integer statusId;
+    private Integer userId;
+    private String fileName;
+    private String fileURL;
+
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getBookingName() {
+        return bookingName;
+    }
+
+    public void setBookingName(String bookingName) {
+        this.bookingName = bookingName;
+    }
+
+    public String getBookingEmail() {
+        return bookingEmail;
+    }
+
+    public void setBookingEmail(String bookingEmail) {
+        this.bookingEmail = bookingEmail;
+    }
+
+    public Date getEventStartTime() {
+        return eventStartTime;
+    }
+
+    public void setEventStartTime(Date eventStartTime) {
+        this.eventStartTime = eventStartTime;
+    }
+
+    public Date getEventEndTime() {
+        return eventEndTime;
+    }
+
+    public void setEventEndTime(Date eventEndTime) {
+        this.eventEndTime = eventEndTime;
+    }
+
+    public Integer getEventDuration() {
+        return eventDuration;
+    }
+
+    public void setEventDuration(Integer eventDuration) {
+        this.eventDuration = eventDuration;
+    }
+
+    public Integer getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(Integer categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public String getEventCategoryName() {
+        return eventCategoryName;
+    }
+
+    public void setEventCategoryName(String eventCategoryName) {
+        this.eventCategoryName = eventCategoryName;
+    }
+
+    public String getEventCategoryDescription() {
+        return eventCategoryDescription;
+    }
+
+    public void setEventCategoryDescription(String eventCategoryDescription) {
+        this.eventCategoryDescription = eventCategoryDescription;
+    }
+
+    public String getEventCategoryDuration() {
+        return eventCategoryDuration;
+    }
+
+    public void setEventCategoryDuration(String eventCategoryDuration) {
+        this.eventCategoryDuration = eventCategoryDuration;
+    }
+
+    public String getEventNotes() {
+        return eventNotes;
+    }
+
+    public void setEventNotes(String eventNotes) {
+        this.eventNotes = eventNotes;
+    }
+
+    public Integer getStatusId() {
+        return statusId;
+    }
+
+    public void setStatusId(Integer statusId) {
+        this.statusId = statusId;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public String getFileURL() {
+        return fileURL;
+    }
+
+    public void setFileURL(String fileURL) {
+        this.fileURL = fileURL;
+    }
+}
