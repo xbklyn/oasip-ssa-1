221ae7c31ccc0abef6e1a7087127de72dca6058c
xbklyn <nut.chotiwit@gmail.com>
2022-10-18 14:31:21 +0700

    edit:add 'ASYNC' annotation for reduce time when create an event

--- oasip-app/src/main/java/sit/int221/oasip/OasipApplication.java
+++ oasip-app/src/main/java/sit/int221/oasip/OasipApplication.java
@@ -3,9 +3,11 @@ package sit.int221.oasip;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.scheduling.annotation.EnableAsync;
 import sit.int221.oasip.properties.FileProperties;
 
 @SpringBootApplication
+@EnableAsync
 @EnableConfigurationProperties({
 		FileProperties.class
 })
